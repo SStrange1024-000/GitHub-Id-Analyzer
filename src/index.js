6ccc40076b1221b6725233e18ddc79d2f0b6ef8f
SStrange1024-000 <136175757+SStrange1024-000@users.noreply.github.com>
2025-11-26 23:46:25 +0530

    Error Solved in src

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <HashRouterRouter>
+    <HashRouter>
     <App />
-    </HashRouterRouter>
+    </HashRouter>
     </Provider>
   </React.StrictMode>
 );
